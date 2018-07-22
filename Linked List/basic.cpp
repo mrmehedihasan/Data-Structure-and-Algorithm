c1bcdb693b5e6955164361bee65db683dc131978
Mehdi Hasan <mehedihasan2002.myc@gmail.com>
2018-07-22 23:16:27 +0600

    Comments-Linked list

--- Linked List/basic.cpp
+++ Linked List/basic.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <malloc.h>
 using namespace std;
-
+//Declaring definition for linked list data type
 typedef struct node Node;
-
+//Setting data to store information and Node for next information memory address
 struct node{
     int data;
     Node *next;
 };
-
+//Print the list of information
 void PrintLinkedList(Node *h){
     while(h!=NULL){
         cout << h->data << endl;
@@ -18,13 +18,17 @@ void PrintLinkedList(Node *h){
 
 
 int main(){
+
+    //Declaring the number of information will stored in linked list
     Node *head = NULL;
     Node *first = NULL;
     Node *second = NULL;
+    //Allocating memory dynamically for information
     head = (Node*)malloc(sizeof(Node));
     first = (Node*)malloc(sizeof(Node));
     second = (Node*)malloc(sizeof(Node));
 
+    //Inserting Data
     head->data = 1;
     head->next = first;
 
@@ -33,7 +37,7 @@ int main(){
 
     second->data = 3;
     second->next = NULL;
-
+    //Printing information
     PrintLinkedList(head);
 
 
